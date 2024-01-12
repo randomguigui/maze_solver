aad0d9726c5d95992b9a747991eb06e9f8d0af7a
randomguigui <arguilh.07@gmail.com>
2024-01-12 20:44:28 +0100

    fixed bugs

--- maze.py
+++ maze.py
@@ -3,6 +3,8 @@ import random
 class Maze:
     def __init__(self, maze: list):
         self.maze = maze
+        self.height = len(self.maze)
+        self.width = len(self.maze[0])
 
     def find_start(self):
         start = ()
@@ -14,58 +16,40 @@ class Maze:
 
     def aside(self, x, y):
         aside = []
-        try:
+
+        if x != self.width - 1:
             if self.maze[y][x + 1] == 0 or self.maze[y][x + 1] == 3:
                 aside.append((y, x + 1))
+        if x != 0:
             if self.maze[y][x - 1] == 0 or self.maze[y][x - 1] == 3:
-                if x - 1 != -1:
-                    aside.append((y, x - 1))
+                aside.append((y, x - 1))
+        if y != 0:
             if self.maze[y - 1][x] == 0 or self.maze[y - 1][x] == 3:
-                if y - 1 != -1:
-                    aside.append((y - 1, x))
+                aside.append((y - 1, x))
+        if y != self.height - 1:
             if self.maze[y + 1][x] == 0 or self.maze[y + 1][x] == 3:
                 aside.append((y + 1, x))
-        except IndexError:
-            pass
 
         return aside
 
     def solution(self):
         start = self.find_start()
-        road = [start]
+        path = [start]
         forbidden = []
 
-        while self.maze[road[-1][0]][road[-1][1]] != 3:
-            aside = self.aside(road[-1][1], road[-1][0])
+        while self.maze[path[-1][0]][path[-1][1]] != 3:
+            aside = self.aside(path[-1][1], path[-1][0])
             len_aside = len(aside)
             for i in range(len_aside):
-                if aside[len_aside - 1 - i] in forbidden or aside[len_aside - 1 - i] in road:
+                if aside[len_aside - 1 - i] in forbidden or aside[len_aside - 1 - i] in path:
                     aside.pop(aside.index(aside[len_aside - 1 - i]))
 
             if not aside:
-                case_pop = road.pop()
+                case_pop = path.pop()
                 forbidden.append(case_pop)
             else:
                 chosen_case_aside = random.choice(aside)
-                road.append(chosen_case_aside)
-        return road
+                path.append(chosen_case_aside)
+        return path
 
-lab = [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [2, 0, 1, 0, 0, 0, 1, 0, 1, 0, 3],
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1]]
-#lab[1][0] = 2
-#lab[8][3] = 3
 
-maze = Maze(lab)
-
-s = maze.solution()
-print(s)
-print(len(s))
